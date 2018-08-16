7fbbd45ad033fbd0aa2516b7c9372daf14299a58
salt <Jinsuke(Lubuntu18.04 on U30S)>
2018-08-16 23:02:53 +0900

    Linuxバージョンを追加

---
+++ init.el
@@ -0,0 +1,30 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;; auto-completeを適用
+(el-get-bundle auto-complete)
+
+;; dracula-themeを使用 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; 行を表示
+(column-number-mode t)
+
+;; 列を表示
+(global-linum-mode t)
+(setq linum-format "%5d| ")
+
+;; C-kで1行切り取り
+(setq kill-whole-line t)
