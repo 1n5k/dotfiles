f7ef5a64069ceaf77e81ba0f1739942a6e9edd1f
Jnsk(Jinsuke) <jnsk@jnsk.info>
2019-07-13 17:51:18 +0900

    Add file: update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOPATH="dotfiles"
+cd $HOME/"DOPATH
+git pull
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    rm -r $HOME/$f
+    ln -snfv "$DOPATH/$f" "$HOME"/"$f"
+done
